7805155d77cf965f8014573c4bf460a774e20e2b
dadofsambonzuki <nathan@day.ag>
2022-05-17 13:11:48 +0100

    Branded Scoreboard

--- Scoreboard/models.py
+++ Scoreboard/models.py
@@ -5,4 +5,3 @@ class Score(models.Model):
     score_value = models.IntegerField(default=0)
     score_date = models.DateTimeField()
     twitter_handle = models.CharField(max_length=50)
-    #user = models.ForeignKey(User, on_delete = models.CASCADE, blank = True, null = True)
