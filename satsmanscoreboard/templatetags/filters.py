4da7cf803e272ed26e123e02e29d7c6fd45f85bc
dadofsambonzuki <nathan@day.ag>
2023-05-17 19:16:42 +0100

    Prettified scores and re-added @ to twitter handles.

--- satsmanscoreboard/templatetags/filters.py
+++ satsmanscoreboard/templatetags/filters.py
@@ -7,3 +7,7 @@ def nostr_short(npub):
     first_eight = npub[:8]
     last_eight = npub[-8:]
     return f"{first_eight}:{last_eight}"
+
+@register.filter
+def pretty_score(score):
+    return f"{score:,}"
