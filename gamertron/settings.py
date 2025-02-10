490efbe341d8b185274ae77092b0e18c0c8f8201
dadofsambonzuki <23073448-dadofsambonzuki@users.noreply.replit.com>
2025-02-10 12:39:17 +0000

    User checkpoint: Fix: Correct static file serving configuration and add admin CSS.

--- gamertron/settings.py
+++ gamertron/settings.py
@@ -134,6 +134,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
