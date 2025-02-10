f6e100a1a619f8393723986fa7164b5ad6046e9a
Nathan Day <87125117+dadofsambonzuki@users.noreply.github.com>
2025-02-10 20:53:29 +0000

    Assistant checkpoint: Add missing TemplateView import
    Assistant generated file changes:
    - gamertron/urls.py: Add TemplateView import
    
    ---
    
    User prompt:
    
    NameError: name 'TemplateView' is not defined

--- gamertron/urls.py
+++ gamertron/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from django.views.generic import RedirectView
+from django.views.generic import RedirectView, TemplateView
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html'), name='home'),
