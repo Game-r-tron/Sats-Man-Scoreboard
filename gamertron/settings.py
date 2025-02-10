a74dc997cc6bf308594a5211421fa82d0a97f2f9
Nathan Day <87125117+dadofsambonzuki@users.noreply.github.com>
2025-02-10 13:16:50 +0000

    Moved imports to top of file

--- gamertron/settings.py
+++ gamertron/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 
+from corsheaders.defaults import default_methods
+from corsheaders.defaults import default_headers
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -155,8 +157,6 @@ REST_FRAMEWORK = {
 
 #CORS Setup
 
-from corsheaders.defaults import default_methods
-from corsheaders.defaults import default_headers
 CORS_ALLOW_METHODS = list(default_methods)
 CORS_ALLOW_HEADERS = list(default_headers)
 CORS_ALLOW_ALL_ORIGINS = True
